11518c5979a2e7ccc0675f2a9bcb592451662498
cdewilliam <williamsadick1996@gmail.com>
2025-09-11 13:52:04 +0300

    Fix Hero component import

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from '../components/Header';
-import { Hero } from '../components/Hero';
+import Hero from '../components/Hero';
 import { Services } from '../components/Services';
 import { Portfolio } from '../components/Portfolio';
 import { About } from '../components/About';
