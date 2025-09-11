b35796e798d43c7e90faacb5fde17a5e2bb3737b
cdewilliam <williamsadick1996@gmail.com>
2025-09-11 13:52:54 +0300

    Fix Services component import

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Header } from '../components/Header';
 import Hero from '../components/Hero';
-import { Services } from '../components/Services';
+import Services from '../components/Services';
 import { Portfolio } from '../components/Portfolio';
 import { About } from '../components/About';
 import { Testimonials } from '../components/Testimonials';
